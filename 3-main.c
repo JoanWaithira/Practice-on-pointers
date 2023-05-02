c0435476a03cd146eb3bf22ea923a36b5a4f03d6
JoanWaithira <joanwaithira.jw@gmail.com>
2023-05-02 00:20:22 -0700

    Types & memory and storing memory addresses

---
+++ 3-main.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+/**
+ * main - printingg the address of the pointer
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int *p;
+
+	printf("Address of variable 'p': %p\n", &p);
+	return (0);
+}
